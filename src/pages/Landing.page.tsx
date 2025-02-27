a990006eda84451b4030a8bfc5e46b7ca0b18db9
Bhargav Mule <bhargavmule16@gmail.com>
2025-02-27 06:45:43 +0530

    Production

--- src/pages/Landing.page.tsx
+++ src/pages/Landing.page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "../components/Navbar";
-import HeroImage2 from "../assets/hero-image-3.jpg";
+import Hero from "../assets/hero-image.jpg"
+import HeroImage3 from "../assets/hero-image-3.jpg"
 import BgGrid from "../assets/bg-grid.avif"
 import { Footer } from "../components/Footer";
 import { Link } from "react-router-dom";
@@ -19,13 +20,13 @@ export function LandingPage(){
             }}
             
             className="flex flex-col justify-center items-center px-4 border-b ">
-            <div className="bg-white/10 backdrop-blur flex flex-col justify-center items-center px-4 w-screen">
+            <div className="bg-white/10 backdrop-blur flex flex-col justify-center items-center px-4 w-screen relative">
 
                     <h1 className="text-6xl font-semibold text-center my-10 tracking-tighter leading-14 md:text-8xl md:leading-22 md:max-w-4xl">
                         Loan Scheduling and Management made Easy
                     </h1>
 
-                    <p className="text-center text-xl text-gray-400 my-4 md:text-2xl md:max-w-4xl">
+                    <p className="text-center text-xl text-gray-400 my-4 md:my-14 md:text-xl md:max-w-4xl">
                     "Tired of complex, time-consuming, frustrating, and inefficient loan schedulers that drain your energy with endless paperwork, confusing calculations, and rigid repayment structures?
                     </p>
 
@@ -51,7 +52,7 @@ export function LandingPage(){
                         One Stop Loan Scheduler and Manager
                     </h1>
 
-                    <p className="text-center mb-10 text-xl text-gray-400 my-4 md:text-2xl md:max-w-4xl">
+                    <p className="text-center mb-10 text-xl text-gray-400 my-4 md:my-14 md:max-w-4xl">
                     Our platform simplifies loan scheduling with seamless automation, effortless customization, and real-time tracking—so you can focus on what truly matters!"
                     </p>
 
@@ -65,7 +66,7 @@ export function LandingPage(){
                             </Link>
                         </div>
 
-                        <img src={HeroImage2} alt="" className="rounded-2xl md:w-full  my-20 md:max-w-5xl "  />
+                        <img src={HeroImage3} alt="" className="rounded-2xl md:w-full  my-20 md:max-w-5xl "  />
 
                     </div>
 
